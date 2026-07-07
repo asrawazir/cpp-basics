302b70a32751045ed18729bf159e2f83b9e42a99
asrawazir <waziralamasra@gmail.com>
2026-07-07 20:10:34 +0500

    Adding funtionality using for loop in SumOfOddNumbers.cpp

--- basics/SumOfOddNumbers.cpp
+++ basics/SumOfOddNumbers.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main()
 {
-    
+    cout << "Enter the number of odd numbers to sum: ";
+    int n;
+    cin >> n;
+
+    int sum = 0;
+    for (int i = 1; i <= n; i += 2) {
+        sum += i;
+    }
+
+    cout << "The sum of the first " << n << " odd numbers is: " << sum << endl;
     return 0;
 }

ac5f921440750fb3d6293a240979946590b872d6
asrawazir <waziralamasra@gmail.com>
2026-07-07 20:27:03 +0500

    Adding functionality of PalindromeChecker.cpp

--- basics/PalindromeChecker.cpp
+++ basics/PalindromeChecker.cpp
@@ -4,5 +4,24 @@ using namespace std;
 int main()
 {
     cout << "--- Palindrome Checker ---" << endl;
+
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    int originalNumber = number;
+    int reversedNumber = 0;
+
+    while (number > 0) {
+        reversedNumber = reversedNumber * 10 + number % 10;
+        number /= 10;
+    }
+
+    if (originalNumber == reversedNumber) {
+        cout << originalNumber << " is a palindrome." << endl;
+    } else {
+        cout << originalNumber << " is not a palindrome." << endl;
+    }
+
     return 0;
 }

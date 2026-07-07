f34f0a77a1bfa7c7e2437fa3aca018d7746a71f6
asrawazir <waziralamasra@gmail.com>
2026-07-07 13:54:00 +0500

    Added the functionality of calculator in Calculator.cpp

--- basics/Calculator.cpp
+++ basics/Calculator.cpp
@@ -19,6 +19,42 @@ int main()
     cout << "3. Multiplication" << endl;
     cout << "4. Division" << endl;
 
-    
+    cout << "Enter your choice: ";
+    int choice;
+    cin >> choice;
+
+    double result;
+
+    if(choice == 1)
+    {
+        result = num1 + num2;
+        cout << "Result: " << result << endl;
+    }
+    else if(choice == 2)
+    {
+        result = num1 - num2;
+        cout << "Result: " << result << endl;
+    }
+    else if(choice == 3)
+    {
+        result = num1 * num2;
+        cout << "Result: " << result << endl;
+    }
+    else if(choice == 4)
+    {
+        if(num2 != 0)
+        {
+            result = num1 / num2;
+            cout << "Result: " << result << endl;
+        }
+        else
+        {
+            cout << "Error: Division by zero is not allowed." << endl;
+        }
+    }
+    else
+    {
+        cout << "Invalid choice." << endl;
+    }
     return 0;
 }

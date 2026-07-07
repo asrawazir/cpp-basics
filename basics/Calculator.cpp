1633d7f08ec9106afeb372ccf8980c495ec3cfa9
asrawazir <waziralamasra@gmail.com>
2026-07-07 13:50:44 +0500

    Added the main body of Calculator in Calculator.cpp

---
+++ basics/Calculator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "--- Calculator ---" << endl;
+
+    cout << "Enter first number: ";
+    double num1;
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    double num2;
+    cin >> num2;
+
+    cout << "Select operations (1-4): ";
+    cout << "1. Addition" << endl;
+    cout << "2. Subtraction" << endl;
+    cout << "3. Multiplication" << endl;
+    cout << "4. Division" << endl;
+
+    
+    return 0;
+}

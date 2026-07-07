63cbe427fad3a98182c31f8f2d15b6e5cf319496
asrawazir <waziralamasra@gmail.com>
2026-07-07 20:16:04 +0500

    Adding funcitonality to check prime number using for loop and conditional statements in PrimeNumberChecker.cpp

--- basics/PrimeNumberChecker.cpp
+++ basics/PrimeNumberChecker.cpp
@@ -4,5 +4,28 @@ using namespace std;
 int main()
 {
     cout << "--- Prime Number Checker ---" << endl;
+
+    cout << "Enter a positive integer: ";
+    int num;
+    cin >> num;
+
+    bool isPrime = true;
+    if (num <= 1) {
+        isPrime = false;
+    } else {
+        for (int i = 2; i <= num - 1; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+
+    if (isPrime) {
+        cout << num << " is a prime number." << endl;
+    } else {
+        cout << num << " is not a prime number." << endl;
+    }
+
     return 0;
 }

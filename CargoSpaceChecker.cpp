89f009563c117650c08cb001e2761787afc15f09
asrawazir <waziralamasra@gmail.com>
2026-07-07 19:49:21 +0500

    Adding processing in CargoSpaceChecker.cpp using while loop

--- CargoSpaceChecker.cpp
+++ CargoSpaceChecker.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 int main()
 {
-    
+    int maxCargoSpace = 1000; // Maximum cargo space in cubic feet
+    cout << "Maximum cargo space: " << maxCargoSpace << " cubic feet" << endl;
+
+    int sum = 0;
+
+    while (sum < maxCargoSpace)
+    {
+        int cargoItem;
+        cout << "Enter the size of the cargo item (in cubic feet): ";
+        cin >> cargoItem;
+        sum += cargoItem;
+    }
     return 0;
 }

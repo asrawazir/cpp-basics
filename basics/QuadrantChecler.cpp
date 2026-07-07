dc8680fed7e1da9b43847ea9cdbdac5e0e0b42f8
asrawazir <waziralamasra@gmail.com>
2026-07-07 13:57:22 +0500

    Added the basic body of Quadrant checker program in QuadrantChecker.cpp

---
+++ basics/QuadrantChecler.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "--- Quadrant Checker ---" << endl;
+    int x, y;
+    cout << "Enter x coordinate: ";
+    cin >> x;
+    cout << "Enter y coordinate: ";
+    cin >> y;
+
+    
+
+    return 0;
+}

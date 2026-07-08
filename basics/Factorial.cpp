bf092639fc366d63e4c84fd88b596bea6781cdb1
asrawazir <waziralamasra@gmail.com>
2026-07-08 20:48:12 +0500

    Added the functionality of Factorial.cpp

--- basics/Factorial.cpp
+++ basics/Factorial.cpp
@@ -8,6 +8,16 @@ int main()
     cout << "Enter a positive integer: ";
     int num;
     cin >> num;
-    
+
+    if (num < 0) {
+        cout << "Please enter a non-negative integer." << endl;
+        return 1;
+    }
+
+    int factorial = 1;
+    for (int i = 1; i <= num; i++) {
+        factorial *= i;
+    }
+    cout << "Factorial of " << num << " is: " << factorial << endl;
     return 0;
 }

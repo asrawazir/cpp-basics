96e8d7bb25a8e2ea5bde650cc77db8a41d575da1
asrawazir <waziralamasra@gmail.com>
2026-07-08 21:00:59 +0500

    Added the body of PrimeNumberFinder.cpp

--- basics/PrimeNumberFinder.cpp
+++ basics/PrimeNumberFinder.cpp
@@ -4,5 +4,18 @@ using namespace std;
 int main()
 {
     cout << "--- Prime Number Finder ---" << endl;
+
+    cout << "Enter the starting number: ";
+    int start;
+    cin >> start;
+
+    cout << "Enter the ending number: ";
+    int end;
+    cin >> end;
+
+    if (start > end) {
+        cout << "Starting number should be less than or equal to ending number." << endl;
+        return 1;
+    }
     return 0;
 }

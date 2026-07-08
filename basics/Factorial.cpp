1655f72903ae3fad0b6e79010c40270b40e5ffa2
asrawazir <waziralamasra@gmail.com>
2026-07-08 20:45:20 +0500

    Added the basic body of Factorial.cpp

---
+++ basics/Factorial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "--- Factorial ---" << endl;
+
+    cout << "Enter a positive integer: ";
+    int num;
+    cin >> num;
+    
+    return 0;
+}

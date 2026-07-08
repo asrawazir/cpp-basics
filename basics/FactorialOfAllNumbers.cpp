44b0edec6ed5c048dfa35e4654e59adccd9ef37e
asrawazir <waziralamasra@gmail.com>
2026-07-08 20:50:37 +0500

    Added the functionality of FactorialOfAllNumbers.cpp using nested loops

--- basics/FactorialOfAllNumbers.cpp
+++ basics/FactorialOfAllNumbers.cpp
@@ -4,5 +4,23 @@ using namespace std;
 int main()
 {
     cout << "--- Factorial Table ---" << endl;
+
+    cout << "Enter a positive integer: ";
+    int num;
+    cin >> num;
+
+    if (num <= 0) {
+        cout << "Please enter a positive integer." << endl;
+        return 1;
+    }
+
+    for (int i = 1; i <= num; i++) {
+        int factorial = 1;
+        for (int j = 1; j <= i; j++) {
+            factorial *= j;
+        }
+        cout << "Factorial of " << i << " is: " << factorial << endl;
+    }
+    
     return 0;
 }

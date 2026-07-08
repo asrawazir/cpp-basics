132c3d87311ee66d688b0cd61a1c1cc3abb1bc40
asrawazir <waziralamasra@gmail.com>
2026-07-08 20:44:05 +0500

    Added the processing for MultiplicationTable.cpp

--- basics/MultiplicationTable.cpp
+++ basics/MultiplicationTable.cpp
@@ -4,5 +4,20 @@ using namespace std;
 int main()
 {
     cout << "--- Multiplication Table ---" << endl;
+
+    cout << "Enter a positive integer: ";
+    int num;
+    cin >> num;
+
+    if (num <= 0) {
+        cout << "Please enter a positive integer." << endl;
+        return 1;
+    }
+
+    cout << "Multiplication Table for " << num << ":" << endl;
+    for (int i = 1; i <= 10; i++) {
+        cout << num << " x " << i << " = " << num * i << endl;
+    }
+
     return 0;
 }

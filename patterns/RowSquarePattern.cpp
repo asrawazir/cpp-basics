7b4fcd9c9b2f18109f4b1a5f5d429ced4198c19d
asrawazir <waziralamasra@gmail.com>
2026-07-09 22:43:33 +0500

    Implemented the funtionality of RowSquarePattern.cpp

--- patterns/RowSquarePattern.cpp
+++ patterns/RowSquarePattern.cpp
@@ -9,6 +9,11 @@ int main()
     int rows;
     cin >> rows;
 
-    
+    for(int i = 1; i <= rows; i++){
+        for(int j = 1; j <= rows; j++){
+            cout << i << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }

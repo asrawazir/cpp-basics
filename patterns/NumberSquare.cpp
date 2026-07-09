1bda535e86c16dcbcd00694a21b64eb8b7ca124b
asrawazir <waziralamasra@gmail.com>
2026-07-09 22:27:26 +0500

    Added the funtionality of NumberSquare.cpp using nested loops

--- patterns/NumberSquare.cpp
+++ patterns/NumberSquare.cpp
@@ -4,5 +4,16 @@ using namespace std;
 int main()
 {
     cout << "--- Number Square ---" << endl;
+
+    cout << "Enter the number of rows: ";
+    int rows;
+    cin >> rows;
+
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < rows; j++){
+            cout << j + 1 << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }

02e4ea33fa816ec51d09eb6652dc1e3978a2015e
asrawazir <waziralamasra@gmail.com>
2026-07-09 22:48:29 +0500

    Added the basic body of InvertedPyramid.cpp

---
+++ patterns/InvertedPyramid.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "--- Inverted Pyramid Pattern ---" << endl;
+
+    cout << "Enter the number of rows: ";
+    int rows;
+    cin >> rows;
+
+    
+    return 0;
+}

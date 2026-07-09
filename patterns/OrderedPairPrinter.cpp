cc6cb1d9219d82d3d2383bc2db411aed05e8a633
asrawazir <waziralamasra@gmail.com>
2026-07-09 22:39:36 +0500

    Implemented the funtionality of OrderedPairPrinter.cpp

--- patterns/OrderedPairPrinter.cpp
+++ patterns/OrderedPairPrinter.cpp
@@ -4,5 +4,20 @@ using namespace std;
 int main()
 {
     cout << "--- Ordered Pair Printer ---" << endl;
+
+    cout << "Enter the range of x: ";
+    int xRange;
+    cin >> xRange;
+
+    cout << "Enter the range of y: ";
+    int yRange;
+    cin >> yRange;
+
+    for(int i = 1; i <= xRange; i++){
+        for(int j = 1; j <= yRange; j++){
+            cout << "(" << i << ", " << j << ") ";
+        }
+        cout << endl;
+    }
     return 0;
 }

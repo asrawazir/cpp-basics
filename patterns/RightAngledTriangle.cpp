67df2ec55ab1b0d492efa18bc661eb0e0b5ec754
asrawazir <waziralamasra@gmail.com>
2026-07-09 22:31:28 +0500

    Implemented the processing of RightAngledTriangle.cpp using nested loops

--- patterns/RightAngledTriangle.cpp
+++ patterns/RightAngledTriangle.cpp
@@ -4,5 +4,16 @@ using namespace std;
 int main()
 {
     cout << "--- Right Angled Star Triangle ---" << endl;
+
+    cout << "Enter the number of rows: ";
+    int rows;
+    cin >> rows;
+
+    for(int i = 0; i < rows; i++){
+        for(int j =0; j <=i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
     return 0;
 }

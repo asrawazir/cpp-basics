bc2062f2cc8fcdf28a1ea2f5e7e1a78abbc24045
asrawazir <waziralamasra@gmail.com>
2026-07-09 22:24:22 +0500

    Added the funtionality if StarSquare.cpp using nested loops

--- patterns/StarSquare.cpp
+++ patterns/StarSquare.cpp
@@ -4,5 +4,18 @@ using namespace std;
 int main()
 {
     cout << "--- Star Square ---" << endl;
+
+    cout << "Enter the number of rows: ";
+    int rows;
+    cin >> rows;
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < rows; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
     return 0;
 }

4b65ea54420608b986de9e84f98f663ce17eb5a1
asrawazir <waziralamasra@gmail.com>
2026-07-09 22:49:15 +0500

    Implemented the funtionality of InvertedPyramid.cpp using nested loops

--- patterns/InvertedPyramid.cpp
+++ patterns/InvertedPyramid.cpp
@@ -9,6 +9,14 @@ int main()
     int rows;
     cin >> rows;
 
-    
+    for(int i = rows; i >= 1; i--){
+        for(int j = 1; j <= rows - i; j++){
+            cout << " ";
+        }
+        for(int k = 1; k <= (2 * i - 1); k++){
+            cout << "*";
+        }
+        cout << endl;
+    }
     return 0;
 }

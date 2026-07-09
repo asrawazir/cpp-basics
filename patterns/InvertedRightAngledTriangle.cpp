7cfef053a2a6bb839f9cbc12612b68da6d92b229
asrawazir <waziralamasra@gmail.com>
2026-07-09 22:35:14 +0500

    Defining the body InvertedRightAngledTriangle.cpp using nested loop

--- patterns/InvertedRightAngledTriangle.cpp
+++ patterns/InvertedRightAngledTriangle.cpp
@@ -9,6 +9,11 @@ int main()
     int rows;
     cin >> rows;
 
-    
+    for(int i = 0; i < rows; i++){
+        for(int j = rows - i; j > 0; j--){
+            cout << "* ";
+        }
+        cout << endl;
+    }
     return 0;
 }

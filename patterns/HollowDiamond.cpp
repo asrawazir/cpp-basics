f65955f85171157c398c819cf47ada2f53a90106
asrawazir <waziralamasra@gmail.com>
2026-07-10 22:59:32 +0500

    Added the complete funtionality in HollowDiamond.cpp

--- patterns/HollowDiamond.cpp
+++ patterns/HollowDiamond.cpp
@@ -24,5 +24,21 @@ int main()
         }
         cout << endl;
     }
+
+    for (int i = rows - 1; i >= 1; i--)
+    {
+        for (int j = 1; j <= rows - i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= (2 * i - 1); k++)
+        {
+            if (k == 1 || k == (2 * i - 1))
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }

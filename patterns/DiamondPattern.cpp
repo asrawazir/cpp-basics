58c4de4053099c0c6f0b07653b87135d2874e2a1
asrawazir <waziralamasra@gmail.com>
2026-07-10 22:48:09 +0500

    Added the funtionality of upper pyramid in DiamondPattern.cpp using nested loops

--- patterns/DiamondPattern.cpp
+++ patterns/DiamondPattern.cpp
@@ -4,5 +4,20 @@ using namespace std;
 int main()
 {
     cout << "-- Diamond Pattern --" << endl;
+
+    cout << "Enter the number of rows: ";
+    int rows;
+    cin >> rows;
+
+    for(int i = 1; i <= rows; i++){
+        for(int j = 1; j <= rows - i; j++){
+            cout << " ";
+        }
+        for(int k = 1; k <= (2 * i - 1); k++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
     return 0;
 }

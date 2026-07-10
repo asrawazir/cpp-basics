211444bcfa1cd8276b58394efe615f28c076adc8
asrawazir <waziralamasra@gmail.com>
2026-07-10 22:58:39 +0500

    Implemented the funtionality of upper half hollow pyramid in HollowDiamond.cpp

--- patterns/HollowDiamond.cpp
+++ patterns/HollowDiamond.cpp
@@ -4,5 +4,25 @@ using namespace std;
 int main()
 {
     cout << "-- Hollow Diamond Pattern --" << endl;
+
+    int rows;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= rows - i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= (2 * i - 1); k++)
+        {
+            if (k == 1 || k == (2 * i - 1))
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }

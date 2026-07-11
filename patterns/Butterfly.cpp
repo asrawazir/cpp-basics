23007a8fc48f8e5c4f01b8b330f130b815b45046
asrawazir <waziralamasra@gmail.com>
2026-07-11 23:10:06 +0500

    Added the funtionality for printing the upper half of butterfly using nested loops in Butterfly.cpp

--- patterns/Butterfly.cpp
+++ patterns/Butterfly.cpp
@@ -4,5 +4,26 @@ using namespace std;
 int main()
 {
     cout << "--- Butterfly Pattern ---" << endl;
+
+    int rows;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int k = 1; k <= (2 * (rows - i)); k++)
+        {
+            cout << " ";
+        }
+        for (int l = 1; l <= i; l++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
     return 0;
 }

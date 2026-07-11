c623083a6257434845a9d5f408040209f0a97ac6
asrawazir <waziralamasra@gmail.com>
2026-07-11 23:11:08 +0500

    Added the funtionality for printing the lower half of the Butterfly.cpp

--- patterns/Butterfly.cpp
+++ patterns/Butterfly.cpp
@@ -25,5 +25,22 @@ int main()
         }
         cout << endl;
     }
+
+    for (int i = rows; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int k = 1; k <= (2 * (rows - i)); k++)
+        {
+            cout << " ";
+        }
+        for (int l = 1; l <= i; l++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
     return 0;
 }

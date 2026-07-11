25a3947fc93e4eeb3bdc8b713b46e6cd486d2e61
asrawazir <waziralamasra@gmail.com>
2026-07-11 23:08:19 +0500

    Added the funtionality of RightAlignedDecreasing.cpp

--- patterns/RightAlignedDecreasing.cpp
+++ patterns/RightAlignedDecreasing.cpp
@@ -9,6 +9,17 @@ int main()
     cout << "Enter the number of rows: ";
     cin >> rows;
 
-
+    for (int i = rows; i >= 1; i--)
+    {
+        for (int j = 1; j <= rows - i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
     return 0;
 }

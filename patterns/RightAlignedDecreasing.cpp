bbdc508ad36a2f309a4da3ba22b681cdf87756ce
asrawazir <waziralamasra@gmail.com>
2026-07-11 23:07:24 +0500

    Added the basic structure of RightAlignedDecreasing.cpp

---
+++ patterns/RightAlignedDecreasing.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "--- Right-Aligned Decreasing Triangle ---" << endl;
+
+    int rows;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+
+    return 0;
+}

390e02b41447427c3346bb414478c3e99e973785
asrawazir <waziralamasra@gmail.com>
2026-07-11 23:06:10 +0500

    Added the funtionality of RightAlignedIncreasing.cpp

--- patterns/RightAlignedIncreasing.cpp
+++ patterns/RightAlignedIncreasing.cpp
@@ -4,5 +4,22 @@ using namespace std;
 int main()
 {
     cout << "--- Right-Aligned Increasing Triangle ---" << endl;
+
+    int rows;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= rows - i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
     return 0;
 }

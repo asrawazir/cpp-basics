663b0b98ef68abff252074788805d1a1fad586ab
asrawazir <waziralamasra@gmail.com>
2026-07-12 23:14:50 +0500

    Implementing the funtionality for adding numbers in the array

---
+++ arrays/InputNumbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numbers[5];
+
+    cout << "Enter 5 numbers: ";
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> numbers[i];
+    }
+    return 0;
+
+
+}

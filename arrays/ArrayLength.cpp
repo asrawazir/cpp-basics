b96f5f8c5458db5fce97817f9a988716d73840ab
asrawazir <waziralamasra@gmail.com>
2026-07-12 23:21:37 +0500

    Adding funtionality for finding the length of the array in ArrayLength.cpp

--- arrays/ArrayLength.cpp
+++ arrays/ArrayLength.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main()
 {
-    
+    int numbers[5] = {1, 2, 3, 4, 5};
+
+    int length = sizeof(numbers) / sizeof(numbers[0]); 
+
+    cout << "Length of the array: " << length << endl;
     return 0;
 }

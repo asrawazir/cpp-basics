d8984d9cb822e70bd5c136e8f986259ae8b10a29
asrawazir <waziralamasra@gmail.com>
2026-07-13 23:07:05 +0500

    Defining the basic structure if SumArray.cpp for initializing an array

---
+++ arrays/SumArray.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    return 0;
+}

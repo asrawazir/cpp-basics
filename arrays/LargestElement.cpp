32e10304765d2c5fbec859b353ad9f83f4368312
asrawazir <waziralamasra@gmail.com>
2026-07-13 23:15:17 +0500

    Add functionality to input array elements in LargestElement.cpp

--- arrays/LargestElement.cpp
+++ arrays/LargestElement.cpp
@@ -6,6 +6,13 @@ int main()
     int n;
     cout << "Enter the number of elements in the array: ";
     cin >> n;
-    
+
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
     return 0;
 }

ab35adc118d6a928ecd387ba861bf8acc7d5897a
asrawazir <waziralamasra@gmail.com>
2026-07-13 23:16:54 +0500

    Add functionality to input array elements in SmallestElement.cpp

--- arrays/SmallestElement.cpp
+++ arrays/SmallestElement.cpp
@@ -7,5 +7,11 @@ int main()
     cout << "Enter the number of elements in the array: ";
     cin >> n;
 
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
     return 0;
 }

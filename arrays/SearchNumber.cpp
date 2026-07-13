ce26a5c348c8e2939b67ddc0656d0f3c490f3366
asrawazir <waziralamasra@gmail.com>
2026-07-13 23:24:46 +0500

    Add functionality to input array elements in SearchNumber.cpp

--- arrays/SearchNumber.cpp
+++ arrays/SearchNumber.cpp
@@ -6,5 +6,14 @@ int main()
     int n;
     cout << "Enter the number of elements in the array: ";
     cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    
+
     return 0;
 }

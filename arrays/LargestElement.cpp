e197721c5f70250fe052dfbef4cb902bf8e03e5f
asrawazir <waziralamasra@gmail.com>
2026-07-13 23:15:49 +0500

    Add functionality to find and display the largest element in the array in LargestElement.cpp

--- arrays/LargestElement.cpp
+++ arrays/LargestElement.cpp
@@ -14,5 +14,15 @@ int main()
         cin >> arr[i];
     }
 
+    int largest = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+    cout << "The largest element in the array is: " << largest << endl;
+
     return 0;
 }

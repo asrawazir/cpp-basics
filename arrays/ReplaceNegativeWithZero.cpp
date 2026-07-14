a409fe7205653c00dd16f80e1d211db9b3f51432
asrawazir <waziralamasra@gmail.com>
2026-07-14 22:16:19 +0500

    Add functionality to display the original array in ReplaceNegativeWithZero.cpp

--- arrays/ReplaceNegativeWithZero.cpp
+++ arrays/ReplaceNegativeWithZero.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 int main()
 {
-    
+    int arr[] = {1, 2, 3, 4, 5, 1, 2, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Original array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     return 0;
 }

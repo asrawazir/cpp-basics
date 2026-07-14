2e1f304d07397f5b6b656ce95b42bee3b034ed1f
asrawazir <waziralamasra@gmail.com>
2026-07-14 22:07:17 +0500

    Add array element input functionality in CountOccurence.cpp

--- arrays/CountOccurence.cpp
+++ arrays/CountOccurence.cpp
@@ -6,5 +6,13 @@ int main()
     int n;
     cout << "Enter the size of the array: ";
     cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    
     return 0;
 }

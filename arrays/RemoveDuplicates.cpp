c829c6d4fd70721617f1048d10742f4775fb5f6f
asrawazir <waziralamasra@gmail.com>
2026-07-14 22:24:22 +0500

    Implement logic to remove duplicates from the array in RemoveDuplicates.cpp

--- arrays/RemoveDuplicates.cpp
+++ arrays/RemoveDuplicates.cpp
@@ -6,12 +6,32 @@ int main()
     int n;
     cout << "Enter the number of elements in the array: ";
     cin >> n;
-    
+
     int arr[n];
     cout << "Enter the elements of the array: ";
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
+    }
+
+
+    int newArr[n];
+    int j = 0;
+    for (int i = 0; i < n; i++)
+    {
+        bool isDuplicate = false;
+        for (int k = 0; k < j; k++)
+        {
+            if (arr[i] == newArr[k])
+            {
+                isDuplicate = true;
+                break;
+            }
+        }
+        if (!isDuplicate)
+        {
+            newArr[j++] = arr[i];
+        }
     }
     return 0;
 }

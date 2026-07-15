c223ace6f6d3cc36b2d76aeb42c3ba1b3f80b608
asrawazir <waziralamasra@gmail.com>
2026-07-15 23:26:58 +0500

    Implement sorting logic and format output in DescendingOrder.cpp

--- arrays/DescendingOrder.cpp
+++ arrays/DescendingOrder.cpp
@@ -13,6 +13,27 @@ int main()
     {
         cin >> arr[i];
     } 
-    
+
+    // Sorting the array in descending order
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] < arr[j + 1])
+            {
+                // Swap arr[j] and arr[j + 1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "The sorted array in descending order is: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     return 0;
 }

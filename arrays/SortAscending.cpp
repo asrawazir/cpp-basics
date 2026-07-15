991df1219b9eb596452e7b4e490ca3a213aa9392
asrawazir <waziralamasra@gmail.com>
2026-07-15 23:16:55 +0500

    Add output for sorted array in SortAscending.cpp

--- arrays/SortAscending.cpp
+++ arrays/SortAscending.cpp
@@ -28,5 +28,11 @@ int main()
             }
         }
     }
+
+    cout << "The sorted array in ascending order is: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
     return 0;
 }

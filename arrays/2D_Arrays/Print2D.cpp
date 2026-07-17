c2f69e08575fff79e9c5dc312a780dfe27b3ffb0
asrawazir <waziralamasra@gmail.com>
2026-07-17 23:25:14 +0500

    Add output display for the 2D array in Print2D.cpp

--- arrays/2D_Arrays/Print2D.cpp
+++ arrays/2D_Arrays/Print2D.cpp
@@ -16,5 +16,13 @@ int main(){
             cin >> arr[i][j];
         }
     }
+
+    cout << "The 2D array is: " << endl;
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }

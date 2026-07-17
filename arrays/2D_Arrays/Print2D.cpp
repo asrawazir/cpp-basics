c9322bca0396983f34ed98671b450464be94db33
asrawazir <waziralamasra@gmail.com>
2026-07-17 23:24:39 +0500

    Add user input for rows and columns in Print2D.cpp

--- arrays/2D_Arrays/Print2D.cpp
+++ arrays/2D_Arrays/Print2D.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main(){
+
+    int rows, cols;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << "Enter the number of columns: ";
+    cin >> cols;
     return 0;
 }

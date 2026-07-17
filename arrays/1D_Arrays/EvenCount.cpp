cfbcbadaf0dd27838779391ba8914b8bc1110576
asrawazir <waziralamasra@gmail.com>
2026-07-17 23:15:58 +0500

    Add DescendingOrder.cpp to sort an array in descending order and EvenCount.cpp to count even elements in an array in 1D_Arrays folder

---
+++ arrays/1D_Arrays/EvenCount.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int evenCount = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            evenCount++;
+        }
+    }
+
+    cout << "The number of even elements in the array is: " << evenCount << endl;
+
+    cout << "The even elements in the array are: ";
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            cout << arr[i] << " ";
+        }
+    }
+    cout << endl;
+    
+    return 0;
+}

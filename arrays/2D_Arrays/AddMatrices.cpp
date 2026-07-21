e2f3109cdc48431566139d150c9713f29e042554
asrawazir <waziralamasra@gmail.com>
2026-07-21 23:32:12 +0500

    Add user input for matrix dimensions in AddMatrices.cpp

--- arrays/2D_Arrays/AddMatrices.cpp
+++ arrays/2D_Arrays/AddMatrices.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 
 int main() {
+    int rows, cols;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << "Enter the number of columns: ";
+    cin >> cols;
     return 0;
 }

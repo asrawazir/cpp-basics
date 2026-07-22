847818a8942d44fc1cacf6f572b61fba317ad833
asrawazir <waziralamasra@gmail.com>
2026-07-22 20:28:32 +0500

    Refactor input handling for matrix dimensions in SubtractMatrices.cpp

--- arrays/2D_Arrays/SubtractMatrices.cpp
+++ arrays/2D_Arrays/SubtractMatrices.cpp
@@ -2,10 +2,14 @@
 using namespace std;
 
 int main() {
-
     const int MAX_ROWS = 100;
     const int MAX_COLS = 100;
 
-    
+     int rows1, cols1;
+    cout << "Enter the number of rows of the first matrix: ";
+    cin >> rows1;
+    cout << "Enter the number of columns of the first matrix: ";
+    cin >> cols1;
+
     return 0;
 }

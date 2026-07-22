9cb6d8eeae5cc40a92d05f6b5b2db2644059ae58
asrawazir <waziralamasra@gmail.com>
2026-07-22 20:36:47 +0500

    Add user input for matrix dimensions in MultiplyMatrices.cpp

--- arrays/2D_Arrays/MultiplyMatrices.cpp
+++ arrays/2D_Arrays/MultiplyMatrices.cpp
@@ -6,5 +6,11 @@ int main() {
     const int MAX_ROWS = 100;
     const int MAX_COLS = 100;
 
+     int rows1, cols1;
+    cout << "Enter the number of rows of the first matrix: ";
+    cin >> rows1;
+    cout << "Enter the number of columns of the first matrix: ";
+    cin >> cols1;
+
     return 0;
 }
